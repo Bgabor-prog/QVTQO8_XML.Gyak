310068c231f9979b72006f6c87523d85856db2cb
Bgabor-prog <demiteto.bartok@gmail.com>
2022-11-25 13:33:44 +0100

    Gyakorlati orak pótlása

--- QVTQO8_1109/xPathQVTQO8.java
+++ QVTQO8_1109/xPathQVTQO8.java
@@ -1,77 +1,156 @@
-import java.io.File;
 import java.io.IOException;
-
-import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
-
 import org.w3c.dom.Document;
-import org.w3c.dom.NodeList;
-import org.w3c.dom.Node;
 import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 public class xPathQVTQO8 {
 
-    public static void main(String[] args) {
+  private static XPath xpath = XPathFactory.newInstance().newXPath();
 
-        try {
-            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder dBuilder;
-            
-            dBuilder = dbFactory.newDocumentBuilder();
-            
-            Document document = dBuilder.parse("QVTQO8_XML.Gyak/QVTQO8_1109/studentQVTQO8.xml");
-            
-            document.getDocumentElement().normalize();
+  public static void main(String[] args) {
+    DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+    try {
+      DocumentBuilder builder = factory.newDocumentBuilder();
 
-            XPath xPath = XPathFactory.newInstance().newXPath();
+      Document doc = builder.parse("./studentQVTQO8.xml");
 
-            String expression = "/class/student";
-            NodeList nodeList = (NodeList) xPath.compile(expression).evaluate(
-                document, XPathConstants.NODESET);
-
-            for (int i = 0; i < nodeList.getLength(); i++) {
-                Node nNode = nodeList.item(i);
-                System.out.println("/nCurrent Element :" + nNode.getNodeName());
-
-                if (nNode.getNodeType() == Node.ELEMENT_NODE) {
-                    Element eElement = (Element) nNode;
-                    System.out.println("Id:" + eElement.getAttribute("id"));
-                    System.out.println("Keresztnev : "
-                            + eElement
-                                    .getElementsByTagName("keresztnev")
-                                    .item(0)
-                                    .getTextContent());
-                    System.out.println("Vezeteknev : "
-                            + eElement
-                                    .getElementsByTagName("vezeteknev")
-                                    .item(0)
-                                    .getTextContent());
-                    System.out.println("Becenev : "
-                            + eElement
-                                    .getElementsByTagName("becenev")
-                                    .item(0)
-                                    .getTextContent());
-                    System.out.println("Kor : "
-                            + eElement
-                                    .getElementsByTagName("kor")
-                                    .item(0)
-                                    .getTextContent());
-                }
-            }
-        } catch (ParserConfigurationException e) {
-            e.printStackTrace();
-        } catch (SAXException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (XPathExpressionException e) {
-            e.printStackTrace();
-        }
+      // printStudents(getAllStudents(doc));
+      // printStudents(getStudentsById(doc, "02"));
+      // printStudents(getSecondStudent(doc));
+      // printStudents(getLastStudent(doc));
+      printStudents(getFirstTwoStudents(doc));
+    } catch (ParserConfigurationException | SAXException | IOException e) {
+      e.printStackTrace();
     }
+  }
+
+  public static NodeList getAllStudents(Document doc) {
+    String expression = "/class/student";
+    try {
+      return (NodeList) xpath
+        .compile(expression)
+        .evaluate(doc, XPathConstants.NODESET);
+    } catch (XPathExpressionException e1) {
+      e1.printStackTrace();
+    }
+
+    return null;
+  }
+
+  public static void printStudents(NodeList nodeList) {
+    for (int i = 0; i < nodeList.getLength(); i++) {
+      Element e = (Element) nodeList.item(i);
+      System.out.println(
+        e.getElementsByTagName("vezeteknev").item(0).getTextContent()
+      );
+      System.out.println(
+        e.getElementsByTagName("keresztnev").item(0).getTextContent()
+      );
+      System.out.println(
+        e.getElementsByTagName("becenev").item(0).getTextContent()
+      );
+      System.out.println(
+        e.getElementsByTagName("kor").item(0).getTextContent()
+      );
+    }
+  }
+
+  public static NodeList getStudentsById(Document doc, String id) {
+    String expression = "/class/student[@id = " + id + "  ]";
+    try {
+      return (NodeList) xpath
+        .compile(expression)
+        .evaluate(doc, XPathConstants.NODESET);
+    } catch (XPathExpressionException e1) {
+      e1.printStackTrace();
+    }
+
+    return null;
+  }
+
+  public static NodeList getSecondStudent(Document doc) {
+    String expression = "/class/student[2]";
+    try {
+      return (NodeList) xpath
+        .compile(expression)
+        .evaluate(doc, XPathConstants.NODESET);
+    } catch (XPathExpressionException e1) {
+      e1.printStackTrace();
+    }
+
+    return null;
+  }
+
+  public static NodeList getLastStudent(Document doc) {
+    String expression = "/class/student[last()]";
+    try {
+      return (NodeList) xpath
+        .compile(expression)
+        .evaluate(doc, XPathConstants.NODESET);
+    } catch (XPathExpressionException e1) {
+      e1.printStackTrace();
+    }
+
+    return null;
+  }
+
+  public static NodeList getFirstTwoStudents(Document doc) {
+    String expression = "/class/student[position() <= 2]";
+    try {
+      return (NodeList) xpath
+        .compile(expression)
+        .evaluate(doc, XPathConstants.NODESET);
+    } catch (XPathExpressionException e1) {
+      e1.printStackTrace();
+    }
+
+    return null;
+  }
+
+  public static NodeList getAllChilds(Document doc) {
+    String expression = "/class";
+    try {
+      return (NodeList) xpath
+        .compile(expression)
+        .evaluate(doc, XPathConstants.NODESET);
+    } catch (XPathExpressionException e1) {
+      e1.printStackTrace();
+    }
+
+    return null;
+  }
+
+  public static NodeList getStudentsByAge(Document doc, int age) {
+    String expression = "/class/student[kor >= " + age + "]";
+    try {
+      return (NodeList) xpath
+        .compile(expression)
+        .evaluate(doc, XPathConstants.NODESET);
+    } catch (XPathExpressionException e1) {
+      e1.printStackTrace();
+    }
+
+    return null;
+  }
+
+  public static NodeList getAllNames(Document doc) {
+    String expression = "/class/student/vezeteknev | /class/student/keresztnev";
+    try {
+      return (NodeList) xpath
+        .compile(expression)
+        .evaluate(doc, XPathConstants.NODESET);
+    } catch (XPathExpressionException e1) {
+      e1.printStackTrace();
+    }
+
+    return null;
+  }
 }
